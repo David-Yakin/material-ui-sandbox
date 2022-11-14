891b040df7be626443193201ac9583a41744b6de
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2022-11-14 12:53:11 +0200

    typography component

---
+++ src/components/DividerComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DividerComponent = () => {
+  return <div>Divider Component works!</div>;
+};
+
+export default DividerComponent;
---
+++ src/components/TypographyComponent.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+const TypographyComponent = () => {
+  return <Typography>One</Typography>;
+};
+
+// const TypographyComponent = () => {
+//   return <Typography variant="h4">Two</Typography>;
+// };
+
+// const TypographyComponent = () => {
+//   return (
+//     <Typography component="h2" variant="h5">
+//       Three
+//     </Typography>
+//   );
+// };
+
+// const TypographyComponent = () => {
+//   return (
+//     <>
+//       <Typography fontWeight={300}>Light</Typography>
+//       <Typography >Regular</Typography>
+//       <Typography fontWeight={500}>Medium</Typography>
+//       <Typography fontWeight={700}>Bold</Typography>
+//     </>
+//   );
+// };
+
+// /***** colors *****/
+// const TypographyComponent = () => {
+//   return (
+//     <>
+//       <Typography color="primary.light">Light</Typography>
+//       <Typography color="primary.main">Main</Typography>
+//       <Typography color="primary.dark">Dark</Typography>
+//       <Typography color="secondary">secondary</Typography>
+//       <Typography color="error">error</Typography>
+//       <Typography color="warning.main">warning</Typography>
+//       <Typography color="info.main">info</Typography>
+//       <Typography color="success.main">success</Typography>
+//       <Typography color="grey">grey</Typography>
+//     </>
+//   );
+// };
+
+// /***** align *****/
+// const TypographyComponent = () => {
+//   return (
+//     <>
+//       <Typography align="center">center</Typography>
+//       <Typography align="left">left</Typography>
+//       <Typography align="right">right</Typography>
+//     </>
+//   );
+// };
+
+// /***** wrap *****/
+// const TypographyComponent = () => {
+//   return (
+//     <>
+//       <Typography>
+//         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores,
+//         architecto. Repellat doloremque maxime minima est temporibus, id quo, ea
+//         aliquid quae earum culpa dolor aut porro ipsum numquam cupiditate fugit!
+//       </Typography>
+//       <hr />
+//       <Typography noWrap>
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo velit
+//         molestiae exercitationem voluptatem quo labore voluptate, incidunt
+//         deserunt commodi necessitatibus. Dolore consectetur necessitatibus
+//         laudantium.
+//       </Typography>
+//     </>
+//   );
+// };
+
+/***** sx *****/
+// const TypographyComponent = () => {
+//   return (
+//     <Typography
+//       sx={{
+//         margin: 2,
+//         backgroundColor: "warning.main",
+//         padding: 2,
+//         textAlign: "center",
+//       }}>
+//       SX
+//     </Typography>
+//   );
+// };
+
+export default TypographyComponent;
